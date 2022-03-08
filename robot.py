5f7ed2613900306a2f9d1b7efb377938ddbfa8f4
Staley Robotics <staleyrobotics@gmail.com>
2022-03-08 15:16:37 -0800

    test

--- robot.py
+++ robot.py
@@ -11,6 +11,7 @@ class Robot(wpilib.TimedRobot):
     motor6 = None
     motor7 = None
     motor8 = None
+    motor9 = None
     controller1 = None
 
     def robotInit(self):
@@ -24,6 +25,7 @@ class Robot(wpilib.TimedRobot):
         self.motor6 = ctre.WPI_VictorSPX(6)
         self.motor7 = ctre.WPI_VictorSPX(7)
         self.motor8 = ctre.WPI_VictorSPX(8)
+        self.motor9 = ctre.WPI_TalonFX(9)
 
         self.controller1 = wpilib.XboxController(0)
 
@@ -33,6 +35,7 @@ class Robot(wpilib.TimedRobot):
             'motor6': self.motor6,
             'motor7': self.motor7,
             'motor8': self.motor8,
+            'motor9': self.motor9,
             'leftDriveMotor1': self.leftDriveMotor1,
             'leftDriveMotor2': self.leftDriveMotor2,
             'rightDriveMotor3': self.rightDriveMotor3,
@@ -55,6 +58,8 @@ class Robot(wpilib.TimedRobot):
         self.motor6.set(0)
         self.motor7.set(0)
         self.motor8.set(0)
+        self.motor9.set(0)
+
         self.motor5.setInverted(True)
         self.motor6.setInverted(True)
         self.leftDriveMotor1.setInverted(True)
@@ -67,9 +72,11 @@ class Robot(wpilib.TimedRobot):
         self.rightDriveMotor3.set(self.controller1.getRightY())
         self.rightDriveMotor4.set(self.controller1.getRightY())
         self.motor5.set(float(self.controller1.getYButton()) * 0.65)
-        self.motor6.set(float(self.controller1.getYButton()) * 1)
+        self.motor6.set(float(self.controller1.getRightTriggerAxis()) * 1)
         self.motor7.set(float(self.controller1.getRightBumper()) * 1)
         self.motor8.set(float(self.controller1.getRightBumper()) * 1)
+        self.motor9.set(float(self.controller1.getAButton()) * 0.2)  #Climber Up
+        self.motor9.set(float(self.controller1.getBButton()) * -0.2)  #Climber Down
         #self.motor5.set(self.controller1.getRightTriggerAxis())
 
 
