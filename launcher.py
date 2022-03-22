016e9f546a396124985604542706c5ccbe69d3f3
Staley Robotics <staleyrobotics@gmail.com>
2022-03-22 12:12:08 -0700

    test

--- launcher.py
+++ launcher.py
@@ -6,6 +6,7 @@ class Launcher:
 
     def __init__(self):
         self.intake_toggle: bool = False
+        #self.intake_pressed: bool = False
         self.shooter = ctre.WPI_TalonFX(5, "rio")
         self.barrel = ctre.WPI_VictorSPX(6)
         self.storage = ctre.WPI_VictorSPX(7)
@@ -15,12 +16,17 @@ class Launcher:
 
         # Toggle intake
         if controller2.getRightBumperPressed():
-            self.intake_toggle = not self.intake_toggle
+            self.intake_toggle = not self.intake_toggle   #Toggle Right Bumper
         self.intake.set(float(self.intake_toggle))
         self.storage.set(float(self.intake_toggle))
 
-        if controller2.getXButton():
+        if controller2.getLeftTrigger() > 0.2:
+            self.intake.set(1)
+
+        if controller2.getLeftBumper():
             self.storage.set(1)
+
+
 
         # Set speed for analog launch
         #if controller2.getRightTriggerAxis():
