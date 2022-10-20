c04a9fc9e5a8ee3876d21ad0bc2519476ec670a8
Hudson <560020@nkcschools.org>
2022-10-20 16:22:25 -0500

    Python 3.10.6 virtual environment and boilerplate code

--- robot.py
+++ robot.py
@@ -1,84 +1,20 @@
 import wpilib
-import ctre
+
 
 class Robot(wpilib.TimedRobot):
 
-    leftDriveMotor1 = None
-    leftDriveMotor2 = None
-    rightDriveMotor3 = None
-    rightDriveMotor4 = None
-    motor5 = None
-    motor6 = None
-    motor7 = None
-    motor8 = None
-    motor9 = None
-    controller1 = None
-
     def robotInit(self):
-
-        self.leftDriveMotor1 = ctre.WPI_TalonFX(1, "rio")
-        self.leftDriveMotor2 = ctre.WPI_TalonFX(2, "rio")
-        self.rightDriveMotor3 = ctre.WPI_TalonFX(3, "rio")
-        self.rightDriveMotor4 = ctre.WPI_TalonFX(4, "rio")
-
-        self.motor5 = ctre.WPI_TalonFX(5, "rio")
-        self.motor6 = ctre.WPI_VictorSPX(6)
-        self.motor7 = ctre.WPI_VictorSPX(7)
-        self.motor8 = ctre.WPI_VictorSPX(8)
-        self.motor9 = ctre.WPI_TalonFX(9)
-
-        self.controller1 = wpilib.XboxController(0)
-
-
-        self.components = {
-            'motor5': self.motor5,
-            'motor6': self.motor6,
-            'motor7': self.motor7,
-            'motor8': self.motor8,
-            'motor9': self.motor9,
-            'leftDriveMotor1': self.leftDriveMotor1,
-            'leftDriveMotor2': self.leftDriveMotor2,
-            'rightDriveMotor3': self.rightDriveMotor3,
-            'rightDriveMotor4': self.rightDriveMotor4
-        }
+        print("Robot Initialized")
 
     def autonomousInit(self):
-        pass
+        print("Started Autonomous")
 
     def autonomousPeriodic(self):
-        #self.automodes.run()
         pass
 
-    def teleopInit(self):
-        self.leftDriveMotor1.set(0)
-        self.leftDriveMotor2.set(0)
-        self.rightDriveMotor3.set(0)
-        self.rightDriveMotor4.set(0)
-        self.motor5.set(0)
-        self.motor6.set(0)
-        self.motor7.set(0)
-        self.motor8.set(0)
-        self.motor9.set(0)
-
-        self.motor5.setInverted(True)
-        self.motor6.setInverted(True)
-        self.leftDriveMotor1.setInverted(True)
-        self.leftDriveMotor2.setInverted(True)
+    def autonomousExit(self):
+        print("Exited Autonomous")
 
 
-    def teleopPeriodic(self):
-        self.leftDriveMotor1.set(self.controller1.getLeftY())
-        self.leftDriveMotor2.set(self.controller1.getLeftY())
-        self.rightDriveMotor3.set(self.controller1.getRightY())
-        self.rightDriveMotor4.set(self.controller1.getRightY())
-        self.motor5.set(float(self.controller1.getYButton()) * 0.65)
-        self.motor6.set(float(self.controller1.getRightTriggerAxis()) * 1)
-        self.motor7.set(float(self.controller1.getRightBumper()) * 1)
-        self.motor8.set(float(self.controller1.getRightBumper()) * 1)
-        self.motor9.set(float(self.controller1.getAButton()) * 0.2)  #Climber Up
-        self.motor9.set(float(self.controller1.getBButton()) * -0.2)  #Climber Down
-        #self.motor5.set(self.controller1.getRightTriggerAxis())
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     wpilib.run(Robot)
